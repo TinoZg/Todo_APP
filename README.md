51195920cea0ba4032dd28538b6c5dc8c523fb3c
TinoZg <valentino.novakk@gmail.com>
2021-10-12 09:10:58 +0200

    bugs with local storage fixed

--- README.md
+++ README.md
@@ -11,8 +11,5 @@ Simple TODO list application. Can be viewed [here](https://tinozg.github.io/Todo
 
 ## Task list
 
-- [x] Items should not be added if input is empty
-- [x] Save data in local storage
-- [ ] Checkbox and deleting from local storage not working properly on load
-- [ ] Add small top padding/margin
+- [ ] Fix bugs with Local Storage
 - [ ] Refactor code
--- functions.js
+++ functions.js
@@ -2,7 +2,7 @@
 // Deletes items from list and local storage
 const appendList = () => {
   // Exit function if input is empty
-  if (document.getElementById("add").value == "") {
+  if (document.getElementById("add").value === "") {
     return;
   }
 
@@ -24,19 +24,28 @@ const appendList = () => {
   checkbox.style.height = "1.1em";
   listItem.appendChild(checkbox);
 
-  // create new input, append it to list item and give it the value from input the form
-  let storageLength = localStorage.length + 1;
+  // create new input, append it to list item and give it the value from input form
+
+  // If local storage is currently empty, add key = item1
+  // If local storage is not empty take "biggest" key value and add 1
+  // e.g. (item1,item2,item3) since item3 is "biggest" new item will be item(3+1) = item4
+  let newStorageKey;
+  if (localStorage.key(0) == null) {
+    newStorageKey = "1";
+  } else {
+    newStorageKey = parseInt(localStorage.key(0).charAt(4)) + 1;
+  }
   const newItem = document.createElement("input");
   newItem.type = "text";
   newItem.readOnly = true;
   newItem.style.backgroundColor = "#fff";
-  newItem.setAttribute("id", "item" + storageLength);
+  newItem.setAttribute("id", "item" + newStorageKey);
   newItem.classList.add("form-control", "mx-3");
   newItem.value = inputValue;
   listItem.appendChild(newItem);
 
   // Add items to local storage
-  localStorage.setItem("item" + storageLength, inputValue);
+  localStorage.setItem("item" + newStorageKey, inputValue);
 
   // button for removing items from the list and local storage
   const removeButton = document.createElement("button");
@@ -61,54 +70,55 @@ const appendList = () => {
 
 // Renders items on page load from local storage
 const renderStorageItems = () => {
-  // Local storage
-  let storageLength = localStorage.length;
-  for (let i = 0; i < storageLength; i++) {
-    // add new list item and append it to the list
-    const list = document.getElementById("list");
-    const listItem = document.createElement("li");
-    listItem.classList.add("list-group-item", "d-flex", "align-items-center");
-    listItem.style.border = "None";
-    list.appendChild(listItem);
+  Object.keys(localStorage)
+    .reverse()
+    .forEach((element) => {
+      const list = document.getElementById("list");
+      const listItem = document.createElement("li");
+      listItem.classList.add("list-group-item", "d-flex", "align-items-center");
+      listItem.style.border = "None";
+      list.appendChild(listItem);
 
-    // create checkbox and append it to list item
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.classList.add("form-check-input");
-    checkbox.style.width = "1.7em";
-    checkbox.style.height = "1.1em";
-    listItem.appendChild(checkbox);
+      // create checkbox and append it to list item
+      const checkbox = document.createElement("input");
+      checkbox.type = "checkbox";
+      checkbox.classList.add("form-check-input");
+      checkbox.style.width = "1.7em";
+      checkbox.style.height = "1.1em";
+      listItem.appendChild(checkbox);
 
-    // if checkbox is checked, change font color to gray
-    checkbox.addEventListener("change", () => {
-      if (checkbox.checked) {
-        newItem.style.color = "#c4c1c0";
-      } else {
-        newItem.style.color = "#212529";
-      }
+      // if checkbox is checked, change font color to gray
+      checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+          newItem.style.color = "#c4c1c0";
+        } else {
+          newItem.style.color = "#212529";
+        }
+      });
+
+      // create new input, append it to list item and give it the value from input form
+      const newItem = document.createElement("input");
+      newItem.type = "text";
+      newItem.readOnly = true;
+      newItem.style.backgroundColor = "#fff";
+      newItem.setAttribute("id", element);
+      newItem.classList.add("form-control", "mx-3");
+      newItem.value = localStorage.getItem(element);
+      listItem.appendChild(newItem);
+
+      // button for removing items from the list
+      const removeButton = document.createElement("button");
+      removeButton.innerHTML = '<i class="bi bi-trash"></i>';
+      listItem.appendChild(removeButton);
+      removeButton.addEventListener("click", () => {
+        localStorage.removeItem(element);
+        listItem.remove();
+        checkbox.remove();
+        removeButton.remove();
+      });
     });
+  // for (let i = 0; i < storageLength; i++) {
+  // add new list item and append it to the list
 
-    // create new input, append it to list item and give it the value from input form
-    const newItem = document.createElement("input");
-    newItem.type = "text";
-    newItem.readOnly = true;
-    newItem.style.backgroundColor = "#fff";
-    newItem.setAttribute("id", "item" + (i + 1));
-    newItem.classList.add("form-control", "mx-3");
-    newItem.value = localStorage.getItem(
-      localStorage.key(storageLength - i - 1)
-    );
-    listItem.appendChild(newItem);
-
-    // button for removing items from the list
-    const removeButton = document.createElement("button");
-    removeButton.innerHTML = '<i class="bi bi-trash"></i>';
-    listItem.appendChild(removeButton);
-    removeButton.addEventListener("click", () => {
-      localStorage.removeItem("item" + (i + 1));
-      listItem.remove();
-      checkbox.remove();
-      removeButton.remove();
-    });
-  }
+  // }
 };
